7c545c32fffd5dbda99268b59469c8c2d5c74cb2
Sven-S. Porst <ssp-web@earthlingsoft.net>
2014-03-23 17:24:47 +0100

    yaz 5.0.21
    * update with changes from main home-brew
    * change icu4c inclusion to only use depends_on (not --with-icu)

--- yaz.rb
+++ yaz.rb
@@ -1,19 +1,37 @@
-require 'formula'
+# -*- coding: UTF-8 -*-
+require "formula"
 
 class Yaz < Formula
-  homepage 'http://www.indexdata.com/yaz'
-  url 'http://ftp.indexdata.dk/pub/yaz/yaz-5.0.19.tar.gz'
-  sha1 '4f267920301e5af5ff2b95243098b75e2da0c9bc'
+  homepage "http://www.indexdata.com/yaz"
+  url "http://ftp.indexdata.dk/pub/yaz/yaz-5.0.21.tar.gz"
+  sha1 "cfa3c49060c15f849a04e73227b4e976370bdbd1"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gnutls' => :optional
-  depends_on 'icu4c'
+  bottle do
+    cellar :any
+    sha1 "e03a4a864ae622fb92b09f7dfc037d3c83477cad" => :mavericks
+    sha1 "d371587eb6dbaadf3a3d2f442a7bc032baed2c43" => :mountain_lion
+    sha1 "f4477ca6a6f7e9018f3fa9cafd8a791fe71feeb7" => :lion
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gnutls" => :optional
+  depends_on "icu4c"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-xml2"
-                          "--with-icu=#{prefix}/opt/icu4c/lib"
     system "make install"
+  end
+
+  # This test converts between MARC8, an obscure mostly-obsolete library
+  # text encoding supported by yaz-iconv, and UTF8.
+  test do
+    marc8 = File.open("marc8.txt", "w") do |f|
+      f.write "$1!0-!L,i$3i$si$Ki$Ai$O!+=(B"
+    end
+
+    result = `"#{bin}/yaz-iconv" -f marc8 -t utf8 marc8.txt`.chomp
+    assert_equal "世界こんにちは！", result
   end
 end
